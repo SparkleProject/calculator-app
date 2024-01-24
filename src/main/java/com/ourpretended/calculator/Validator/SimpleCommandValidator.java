8dc808dfb183909799facac7d8a290e2435498eb
EricLin <lincwei@foxmail.com>
2024-01-24 17:21:25 +1300

    cleanup

---
+++ src/main/java/com/ourpretended/calculator/Validator/SimpleCommandValidator.java
@@ -0,0 +1,18 @@
+package com.ourpretended.calculator.Validator;
+
+import com.ourpretended.calculator.exception.IllegalExpressionException;
+
+import static com.ourpretended.calculator.config.ApplicationConstants.OPERAND_REGEX;
+import static com.ourpretended.calculator.config.ApplicationConstants.OPERATION_REGEX;
+
+public final class SimpleCommandValidator {
+
+    private final static String COMMAND_SINGLE_OPERATION = OPERAND_REGEX+"\\s+"+OPERATION_REGEX+"\\s+"+OPERAND_REGEX;
+
+    public static void validate(String input){
+        if(!input.matches(COMMAND_SINGLE_OPERATION)){
+            throw new IllegalExpressionException("Illegal command inputs.");
+        }
+    }
+
+}
