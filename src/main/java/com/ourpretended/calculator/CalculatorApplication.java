ae078d346c055685366df7b200f06e2b9c4eeab1
EricLin <lincwei@foxmail.com>
2024-01-25 09:57:52 +1300

    add invoke example

---
+++ src/main/java/com/ourpretended/calculator/CalculatorApplication.java
@@ -0,0 +1,26 @@
+package com.ourpretended.calculator;
+
+import com.ourpretended.calculator.config.OperationContext;
+import com.ourpretended.calculator.factory.OperationFactory;
+import com.ourpretended.calculator.service.SimpleCommandResolver;
+import com.ourpretended.calculator.validator.SimpleCommandValidator;
+
+public class CalculatorApplication {
+
+    // Invoke example
+    public static void main(String[] args) {
+
+        // Application Initialize
+        OperationContext context = new OperationContext();
+        SimpleCommandValidator commandValidator = new SimpleCommandValidator();
+        SimpleCommandResolver resolver = new SimpleCommandResolver(context, commandValidator);
+        OperationFactory factory = new OperationFactory(context);
+        CalculatorHandler calculator = new CalculatorHandler(resolver, factory);
+
+        // invoke
+        String input = "4.6 + 5.8";
+        String result = calculator.calculate(input);
+        System.out.println(result);
+
+    }
+}
